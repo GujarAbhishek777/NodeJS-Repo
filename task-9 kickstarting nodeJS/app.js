0ad0776867ba5b3b893d02d9e4ee97d8e7909557
Abhishek Gujar <gujarabhishek777@gmail.com>
2023-01-03 20:03:40 +0530

    task 6 serving html css

--- task-9 kickstarting nodeJS/app.js
+++ task-9 kickstarting nodeJS/app.js
@@ -1,3 +1,5 @@
+const path=require('path');
+
 const express = require('express');
 
 const app = express();
@@ -10,10 +12,10 @@ const bodyParser=require('body-parser');
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/admin',adminRoutes);
-app.use('/shop',shopRoutes);
+app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-   res.status(404).send('<h1>Page not found</h1>');
+   res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 
 app.listen(3000);
--- task-9 kickstarting nodeJS/routes/shop.js
+++ task-9 kickstarting nodeJS/routes/shop.js
@@ -1,15 +1,21 @@
+
+const path=require('path');
+
 const express=require('express');
+
+const rootDir=require('../util/path')
+
 
 const router=express.Router();
 
-router.get('/add-product', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>');
-  });
+router.get('/', (req, res, next) => {
+    res.sendFile(path.join(rootDir,'views','shop.html'));
+});
 
-  router.post('/add-product',(req,res,next)=>{
-    console.log(req.body);
+//   router.post('/add-product',(req,res,next)=>{
+//     console.log(req.body);
     
-    res.redirect('/');
-  });
+//     res.redirect('/');
+//   });
 
 module.exports=router;
